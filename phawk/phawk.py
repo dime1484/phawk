62bde64c5fcbd2db53707957ff8888efbce3afe9
Dimitrije <101292834+dime1484@users.noreply.github.com>
2022-04-24 23:42:29 +0200

    Create phawk.py

---
+++ phawk/phawk.py
@@ -0,0 +1,49 @@
+import socket
+
+class UDP:
+    def __init__(self, **kwargs):
+        self.ip = kwargs.get('ip')
+        self.port = kwargs.get('port')
+        self.listen_ip = kwargs.get('lip')
+        self.listen_port = kwargs.get('lport')
+
+    def send(self, message):
+        '''
+        Simply sends UDP packet to the
+        set ip and port, no questions
+        asked.
+        '''
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.sendto(message.encode('utf-8'), (self.ip, self.port))
+        print(f"hawk.send {message} -> [{self.ip}]")
+
+    def get(self, **kwargs):
+        ''' 
+        Listens for UDP requests on the
+        previously set listen_ip and listen_port.
+        Ignores ip and port unless the excl keyword
+        argument is set to boolean True.
+        '''
+        
+        buffer = kwargs.get('buffer')
+        exclusive = kwargs.get('excl')
+
+        # [NEEDS MORE TESTING LATER] if exclusive: # check if exclusive kwarg is set to true
+        '''
+            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            sock.bind(self.lip, self.lport)
+            while True:
+                received_data, address = sock.recvfrom(int(buffer))
+                if (address[0] == self.ip) and (address[1] == self.port):
+                    print(f"[{address}] -> hawk.get: {received_data}")    
+                else:
+                    pass
+            sock.close()
+        '''
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.bind((self.listen_ip, self.listen_port))
+        while True:
+            received_data, address = sock.recvfrom(int(buffer))
+            print(f"[{address}] -> hawk.get: {received_data.decode('utf-8')}")
+        sock.close()
+
